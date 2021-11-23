3abe56293e0e052e9bda7d440c2309da13c71e97
liangjuchao <liangjuchao@valuprosys.com>
2021-11-23 14:07:43 +0800

    initialize

---
+++ src/main/java/com/liangjuchao/InitiateApplication.java
@@ -0,0 +1,13 @@
+package com.liangjuchao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitiateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitiateApplication.class,args);
+    }
+
+}
